148cebf8bdeeeff3f49b17f9b3646ed4c3f9658b
eduardo <eduardo@DEBIAN.root404.com>
2016-04-07 19:23:20 -0500

    Fixed an autoload issue

--- autoload.php
+++ autoload.php
@@ -7,9 +7,9 @@
 $loader = null;
 
 // if composer loader exists, use it
-if(file_exists('vendor/autoload.php')) {
-    $loader = require 'vendor/autoload.php';
+if(file_exists('../vendor/autoload.php')) {
+    $loader = require '../vendor/autoload.php';
 } else {
    // if not, use the bitphp's loader
-    $loader = require 'bitphp/autoload.php';
+    $loader = require '../bitphp/autoload.php';
 }
